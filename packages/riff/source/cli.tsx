3fd4a07179ee39ed19dfb51a88d9dcb3182cd48d
James Darrohn <oblongobjects.hq@gmail.com>
2021-04-07 23:08:09 -0600

    Started riff CLI

---
+++ packages/riff/source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ riff
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ riff --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
