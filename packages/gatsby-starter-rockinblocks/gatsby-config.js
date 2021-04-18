1cd732ca99a083d83f68e34b17ccef762824d9bc
James Darrohn <oblongobjects.hq@gmail.com>
2021-04-17 23:25:48 -0600

    Change favicon and bump block version

--- packages/gatsby-starter-rockinblocks/gatsby-config.js
+++ packages/gatsby-starter-rockinblocks/gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/johnny-cash-mugshot.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/rockinblocks-logo.jpg`, // This path is relative to the root of the site.
       },
     },
     {
