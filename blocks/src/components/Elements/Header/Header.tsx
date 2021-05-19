0596629b26a09c37f66faa1ee6668dde4c178a78
Jed Darrohn <jamesdarrohn@gmail.com>
2021-05-18 23:16:05 -0600

    Cleaning up Header. Fixing flexDirection error

--- blocks/src/components/Elements/Header/Header.tsx
+++ blocks/src/components/Elements/Header/Header.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { useSettings } from "../../Utilities/Settings";
 import { Navbar } from "@rockinblocks/gatsby-plugin-rockinblocks";
 
-const Header: React.FC = (): JSX.Element => {
-  const { appName } = useSettings();
-
+export const Header: React.FC = (): JSX.Element => {
   return (
     <header>
       <div className="header">
-        <Navbar appName={appName} color={"#ffffff"} backgroundColor="#ffffff" />
+        <Navbar color={"#ffffff"} backgroundColor="#ffffff" />
       </div>
     </header>
   );
