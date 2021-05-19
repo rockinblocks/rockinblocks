82d8cfa7eb4fac6364cb7ca2714112ae7bdb08b6
Jed Darrohn <jamesdarrohn@gmail.com>
2021-05-19 00:56:09 -0600

    Adding GTM field and app version field to settings

--- sites/gatsby/rockinblocks/src/components/Utilities/Settings/formConfig.js
+++ sites/gatsby/rockinblocks/src/components/Utilities/Settings/formConfig.js
@@ -8,6 +8,18 @@ export const formConfig = {
       description: 'The name of your application.',
       component: 'text',
     },
+    {
+      label: 'App Version',
+      name: 'rawJson.appVersion',
+      description: 'The application verison.',
+      component: 'text',
+    },
+    {
+      label: 'Google Tag Manager ID',
+      name: 'rawJson.gtmId',
+      description: 'Google Tag Manager Property ID',
+      component: 'text',
+    },
   ]
 }
 
