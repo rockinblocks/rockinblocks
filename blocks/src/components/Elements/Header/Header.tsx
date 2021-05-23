bdc54b124525edd58c14449e95ac1bea564e6b07
Jed Darrohn <jamesdarrohn@gmail.com>
2021-05-22 19:02:02 -0600

    Adding Header to Elements, though not implementing in Site yet

---
+++ blocks/src/components/Elements/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar } from "../Navbar";
+
+export const Header: React.FC = (): JSX.Element => {
+  return (
+    <header>
+      <div className="header">
+        <Navbar />
+      </div>
+    </header>
+  );
+};
+
+export const HeaderForm = {
+  id: "rockin-docs-header",
+  label: "Header",
+  fields: [
+    {
+      label: "App Name",
+      name: "rawJson.appName",
+      description: "Name of the app",
+      component: "text",
+    },
+  ],
+  onSubmit: async () => {
+    window.alert("Saved!");
+  },
+};
+
+export default Header;
