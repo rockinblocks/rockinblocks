d13180eece8802fe80fed9128215bce6153b39ae
Jed Darrohn <jamesdarrohn@gmail.com>
2021-05-23 00:11:36 -0600

    Fix parser issue in .eslintrc.js

--- blocks/.eslintrc.js
+++ blocks/.eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
   extends: ["xo", "xo-react", "xo-typescript", "prettier"],
-  parser: "@typescript/eslint-parser",
+  parser: "@typescript-eslint/parser",
   env: {
     browser: true,
     node: true,
     jest: true,
   },
   parserOptions: {
+    project: "./tsconfig.json",
     ecmaVersion: 11,
     sourceType: "module",
     ecmaFeatures: {
