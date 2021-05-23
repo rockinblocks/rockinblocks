1704fa0acf33dcaa77eeff7136ed80bdfbf1d0c7
Jed Darrohn <jamesdarrohn@gmail.com>
2021-05-22 19:03:42 -0600

    Removing the problematic props spreader from our Layout components.

--- blocks/src/components/Layout/Col/Col.tsx
+++ blocks/src/components/Layout/Col/Col.tsx
@@ -1,17 +1,17 @@
-import React, { CSSProperties, ReactNode } from 'react';
-import styles from './Col.scss';
+import React, { CSSProperties, ReactNode } from "react";
+import styles from "./Col.scss";
 
 export interface IColProps {
-	children?: ReactNode;
-	display?: string;
-	flex?: number | string;
-	flexDirection?: string;
-	justifyContent?: string;
-	style?: CSSProperties;
+  children?: ReactNode;
+  display?: string;
+  flex?: number | string;
+  flexDirection?: string;
+  justifyContent?: string;
+  style?: CSSProperties;
 }
 
 export const Col: React.FC<
-IColProps & React.HTMLAttributes<HTMLDivElement> & CSSProperties
+  IColProps & React.HTMLAttributes<HTMLDivElement> & CSSProperties
 > = props => {
 	const { display, flex, flexDirection, justifyContent, children, style } =
     props;
@@ -23,7 +23,7 @@ IColProps & React.HTMLAttributes<HTMLDivElement> & CSSProperties
 		...style
 	};
 	return (
-		<div className={styles.col} style={{ ...styleProps }} {...props}>
+		<div className={styles.col} style={{ ...styleProps }}>
 			{children}
 		</div>
 	);
--- blocks/src/components/Layout/Container/Container.tsx
+++ blocks/src/components/Layout/Container/Container.tsx
@@ -1,38 +1,32 @@
-import React, { CSSProperties, ReactNode } from 'react';
-import styles from './Container.scss';
+import React, { CSSProperties, ReactNode } from "react";
+import styles from "./Container.scss";
 
 export interface IContainerProps {
-	children?: ReactNode;
-	display?: string;
-	flex?: number | string;
-	flexDirection?: string;
-	justifyContent?: string;
-	style?: CSSProperties;
+  children?: ReactNode;
+  display?: string;
+  flex?: number | string;
+  flexDirection?: string;
+  justifyContent?: string;
+  style?: CSSProperties;
 }
 
 export const Container: React.FC<
-IContainerProps & React.HTMLAttributes<HTMLDivElement> & CSSProperties
+  IContainerProps & React.HTMLAttributes<HTMLDivElement> & CSSProperties
 > = props => {
-	const {
-		display,
-		flex,
-		flexDirection,
-		justifyContent,
-		children,
-		style
-	} = props;
-	const styleProps = {
-		display,
-		flex,
-		flexDirection,
-		justifyContent,
-		...style
-	};
-	return (
-		<div className={styles.container} style={{ ...styleProps }} {...props}>
-			{children}
-		</div>
-	);
+  const { display, flex, flexDirection, justifyContent, children, style } =
+    props;
+  const styleProps = {
+    display,
+    flex,
+    flexDirection,
+    justifyContent,
+    ...style,
+  };
+  return (
+    <div className={styles.container} style={{ ...styleProps }}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
