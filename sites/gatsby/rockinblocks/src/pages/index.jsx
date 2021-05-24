fc9669a60f22762a984c5cd92918a90f2c92d2a3
Jed Darrohn <jamesdarrohn@gmail.com>
2021-05-23 22:34:54 -0600

    Home page just has the hardcoded Hero

--- sites/gatsby/rockinblocks/src/pages/index.jsx
+++ sites/gatsby/rockinblocks/src/pages/index.jsx
@@ -2,8 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/mainLayout"
 import { ContextProvider, SEO } from "../components/Utilities"
-import { Blocks } from "../components/Builder"
-import { Footer } from "@rockinblocks/gatsby-plugin-rockinblocks"
+import { Footer, Hero } from "@rockinblocks/gatsby-plugin-rockinblocks"
 
 const Home = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -12,7 +11,13 @@ const Home = ({ data, location }) => {
     <ContextProvider>
       <Layout location={location} title={siteTitle}>
         <SEO title="A practical Yarn workspace powered Gatsby, Storybook, and Tina CMS for building a block library in React." />
-        <Blocks />
+        <Hero
+          heading="Rock out with your blocks out."
+          seoHeading="An open-Gatsby workspace with documentation-driven development in mind, focused on improving startup success."
+          subheading="Create your own block library in React. Rockin' Blocks is built with one of the fastest web frameworks, GatsbyJS, which means when it comes to performance, Rockin' Blocks can really shred."
+          buttonText="Start a Project"
+          buttonLink="/docs/v0/introduction"
+        />
         <Footer />
       </Layout>
     </ContextProvider>
