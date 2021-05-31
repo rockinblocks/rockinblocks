0a1097aa88061f5e53fcc2e49c075a8024f17dd4
Jed Darrohn <jamesdarrohn@gmail.com>
2021-05-30 19:32:12 -0600

    Linting on mainLayout.js

--- sites/gatsby/rockinblocks/src/components/mainLayout.js
+++ sites/gatsby/rockinblocks/src/components/mainLayout.js
@@ -5,16 +5,16 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import Header from '../components/Layout/Header/Header'
+import React from "react"
+import PropTypes from "prop-types"
+import Header from "../components/Layout/Header/Header"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <div className="layout-wrapper">
-          <main>{children}</main>
+        <main>{children}</main>
         {/* <Footer /> */}
       </div>
     </>
