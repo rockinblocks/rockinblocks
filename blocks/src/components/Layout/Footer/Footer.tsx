fbb281296c0451c1ba9763176f57979b6459102b
Jed Darrohn <jamesdarrohn@gmail.com>
2021-06-04 23:16:34 -0600

    Handling cookie acceptance with Alert in footer

--- blocks/src/components/Layout/Footer/Footer.tsx
+++ blocks/src/components/Layout/Footer/Footer.tsx
@@ -1,9 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Link } from "gatsby";
 import { Box, Col, Container, Row } from "..";
+import { Alert } from "../../Elements"
 import styles from "./Footer.scss";
 
 export const Footer: FC = (): JSX.Element => {
+  const [acceptedCookies, setAcceptedCookies] = useState(true);
+
+  const handleAcceptedCookies = () => {
+    window.localStorage.setItem('rockinblocks_cookies', String(new Date().getTime()) );
+    setAcceptedCookies(true);
+  }
+
+  useEffect(() => {
+    const accepted = window.localStorage.getItem('rockinblocks_cookies');
+    console.log({
+      accepted
+    })
+    if (!accepted) {
+      setAcceptedCookies(false);
+    }
+  }, [])
+
   return (
     <div>
       <div className={styles.footer}>
@@ -26,6 +44,16 @@ export const Footer: FC = (): JSX.Element => {
           </Row>
         </Container>
       </div>
+      {!acceptedCookies &&
+        <div style={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
+          <Alert
+            text="This site uses cookies to improve the Rockin' Blocks user experience."
+            type="info"
+            link={{ text: "Accept Cookies", location: "/privacy-policy" }}
+            onClick={() => handleAcceptedCookies}
+          />
+        </div>
+      }
     </div>
   );
 };
