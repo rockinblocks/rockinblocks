4fc50eb9eb2b19afa27ac26aa5264aac364fe6b2
James Darrohn <oblongobjects.hq@gmail.com>
2022-11-11 15:23:37 -0700

    Working on main Hero on home page

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -38,14 +38,6 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: "/src/images/" // See below to configure properly
-        }
-      }
     }
   ],
 }
