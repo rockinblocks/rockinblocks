19fe30e9c82fc8fcbfa5e327164d46015dfea1aa
James Darrohn <oblongobjects.hq@gmail.com>
2022-11-15 05:47:31 -0700

    Shadow on button

--- blocks/lib/components/Elements/Button.tsx
+++ blocks/lib/components/Elements/Button.tsx
@@ -9,11 +9,12 @@ export interface IButtonProps {
   to?: string;
   href?: string;
   onClick?: (event: MouseEvent) => void;
+  className?: string;
 }
 
 export const Button: FC<IButtonProps> = props => {
   return (
-      <button className="bg-red-500 p-4 lg:w-48 rounded-full">
+      <button className="bg-red-500 p-4 lg:w-48 rounded-full drop-shadow">
         <span className="text-white font-bold">
           Start a Project
         </span>
